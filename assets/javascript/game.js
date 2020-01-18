a3aa1c1014358b0b867d8c3d244a26f7bc57d6d4
Emily M <30880308+e0marina@users.noreply.github.com>
2020-01-18 15:45:42 -0800

    script added for gen random num

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,19 @@
-// GLOBAL VARIABLES
+// GLOBAL VARIABLES (variables we want to be avails to whole game)
 //=======================================================================
-
+var wins = 0;
+var losses = 0;
 //FUNCTIONS
 //=======================================================================
+//at game start/reset - generate a random number (that user will try to match)
+//and display it on the screen
+function randomNum(min, max) {
+  return Math.floor(Math.random() * (120 - 19)) + 19;
+}
+$("#random-num-text").html(randomNum);
+
+//at game start/reset - diff random numbers assigned to crystals
+//at game start/reset - total score set to 0
+//if player matches random number they win else they go above and lose and the counters update
 
 //MAIN PROCESS
 //=======================================================================
