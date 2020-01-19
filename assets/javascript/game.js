96e8b3741e9c47c433ed5d43292205835fbd27e9
Emily M <30880308+e0marina@users.noreply.github.com>
2020-01-18 21:42:06 -0800

    moved math randomNum to global var

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,44 +2,75 @@
 //=======================================================================
 var wins = 0;
 var losses = 0;
+var scoreBox = 0;
+//The below seems redundant...no?
+var crystalBlue = Math.floor(Math.random() * (12 - 1)) + 1;
+var crystalPink = Math.floor(Math.random() * (12 - 1)) + 1;
+var crystalPurple = Math.floor(Math.random() * (12 - 1)) + 1;
+var crystalRed = Math.floor(Math.random() * (12 - 1)) + 1;
+//randomNum
+function randomNum(min, max) {
+  return Math.floor(Math.random() * (120 - 19)) + 19;
+}
 //FUNCTIONS
 //=======================================================================
 //at game start/reset - generate a random number (that user will try to match)
 //and display it on the screen
 function gameStart() {
-  function randomNum(min, max) {
-    return Math.floor(Math.random() * (120 - 19)) + 19;
-  }
   $("#random-num-text").html(randomNum);
-  //define function to generate random nums to crystals, do 4x
-  var printCrystalNum;
-  function crystalNum(min, max) {
-    return Math.floor(Math.random() * (12 - 1)) + 1;
-  }
+  //define function to generate random nums to crystals
 
   //at game start/reset - diff random numbers assigned to crystals
-  $("#blue").html(crystalNum);
+
+  $("#blue").html(crystalBlue);
   console.log("bluenum");
 
-  $("#pink").html(crystalNum);
+  $("#pink").html(crystalPink);
   console.log("pinknum");
 
-  $("#purple").html(crystalNum);
+  $("#purple").html(crystalPurple);
   console.log("purplenum");
 
-  $("#red").html(crystalNum);
+  $("#red").html(crystalRed);
   console.log("rednum");
+
   //at game start/reset - total score set to 0
+
   $("#score-box").html("0");
   console.log("score box working");
 }
 gameStart();
 
-//if player matches random number they win else they go above and lose and the counters update
-if (condition) {
-} else {
-}
-
 //MAIN PROCESS
 //=======================================================================
-//when each crystal is clicked, add its value to the total score
+// //when each crystal is clicked, add its value to the total score
+$("#blue").on("click", function() {
+  scoreBox = scoreBox + crystalBlue;
+  //show in counter area
+  $("#score-box").html(scoreBox);
+});
+$("#pink").on("click", function() {
+  scoreBox = scoreBox + crystalPink;
+  //show in counter area
+  $("#score-box").html(scoreBox);
+});
+$("#purple").on("click", function() {
+  scoreBox = scoreBox + crystalPurple;
+  //show in counter area
+  $("#score-box").html(scoreBox);
+});
+$("#red").on("click", function() {
+  scoreBox = scoreBox + crystalRed;
+  //show in counter area
+  $("#score-box").html(scoreBox);
+});
+//if user's score equals the random number, user wins, yay
+if (randomNum === scoreBox) {
+  wins++;
+  $("#wins-p").html(wins);
+} else if (randomNum < scoreBox) {
+  losses++;
+  $("losses-p").html(losses);
+} else {
+}
+//if user's score goes above the random number, user loses
