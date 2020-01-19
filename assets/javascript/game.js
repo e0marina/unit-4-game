a4b0b2089391b2a61ebf8916acc550db13ea75d5
Emily M <30880308+e0marina@users.noreply.github.com>
2020-01-18 16:30:33 -0800

    trying to figure out assigning random numbers to crystals

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,14 +6,40 @@ var losses = 0;
 //=======================================================================
 //at game start/reset - generate a random number (that user will try to match)
 //and display it on the screen
-function randomNum(min, max) {
-  return Math.floor(Math.random() * (120 - 19)) + 19;
-}
-$("#random-num-text").html(randomNum);
+function gameStart() {
+  function randomNum(min, max) {
+    return Math.floor(Math.random() * (120 - 19)) + 19;
+  }
+  $("#random-num-text").html(randomNum);
+  //define function to generate random nums to crystals, do 4x
+  var printCrystalNum;
+  function crystalNum(min, max) {
+    return Math.floor(Math.random() * (12 - 1)) + 1;
+  }
 
-//at game start/reset - diff random numbers assigned to crystals
-//at game start/reset - total score set to 0
+  //at game start/reset - diff random numbers assigned to crystals
+  $("#blue").html(crystalNum);
+  console.log("bluenum");
+
+  $("#pink").html(crystalNum);
+  console.log("pinknum");
+
+  $("#purple").html(crystalNum);
+  console.log("purplenum");
+
+  $("#red").html(crystalNum);
+  console.log("rednum");
+  //at game start/reset - total score set to 0
+  $("#score-box").html("0");
+  console.log("score box working");
+}
+gameStart();
+
 //if player matches random number they win else they go above and lose and the counters update
+if (condition) {
+} else {
+}
 
 //MAIN PROCESS
 //=======================================================================
+//when each crystal is clicked, add its value to the total score
